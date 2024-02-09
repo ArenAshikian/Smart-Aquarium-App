30bbfbb6239656fe797c80ce73ddb76312bb8055
ArenAshikian <ashikianaren@gmail.com>
2024-02-08 19:24:02 -0800

    First Commit

---
+++ Smart-Aquarium-App/Smart_Aquarium_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_Aquarium_AppApp.swift
+//  Smart-Aquarium-App
+//
+//  Created by csuftitan on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Smart_Aquarium_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
